6cbc21ca61b0bd80b74d8d7969857cf757c44dda
Dudhatkaran <karanvd2002@gmail.com>
2023-06-17 17:51:43 +0530

    start

---
+++ formik-demo-main/src/App.js
@@ -0,0 +1,41 @@
+import './App.css';
+import { useFormik } from "formik";
+import { valiDateSchema } from './Schema'
+
+function App() {
+  const valuesForm = {
+    name: '',
+    password: ''
+  }
+  const { handleChange, values, handleSubmit } = useFormik({
+    initialValues: valuesForm,
+    validationSchema: valiDateSchema,
+    onSubmit: (values) => {
+      console.log('values::: ', values);
+    }
+  })
+
+  return (
+    <>
+      <div className="form">
+        <form onSubmit={ handleSubmit }>
+          <input
+            type="name"
+            name='name'
+            id='name'
+            onChange={ handleChange }
+            value={ values.email } />
+          <input
+            type="password"
+            name='password'
+            id='password'
+            onChange={ handleChange }
+            value={ values.password } />
+          <button type='submit'>Submit</button>
+        </form>
+      </div>
+    </>
+  );
+}
+
+export default App;
